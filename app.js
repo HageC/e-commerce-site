13250c980293e18198c66436309311c033901161
Christopher Hage <hage.chris@yahoo.com>
2022-11-04 19:43:18 -0300

    login controller started

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ import morgan from "morgan";
 import notFound from "./middleware/not-found.js";
 import errorHandler from "./middleware/error-handler.js";
 import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 const port = process.env.PORT || 5000;
 const app = express();
 
 //app.use(morgan("tiny"));
 app.use(express.json());
-
+app.use(cookieParser(process.env.JWT_SECRET));
 app.get("/", (req, res) => {
+  console.log(req.signedCookies);
   res.send("Hello World.");
 });
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -7,19 +7,20 @@ const register = async (req, res, next) => {
     return next(new CustomError("Please enter all values.", 400));
   }
 
-  const userExists = await User.findOne({ email });
-
-  if (userExists) {
-    return next(new CustomError("Email is already in use.", 400));
-  }
-
   try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return next(new CustomError("Email is already in use.", 400));
+    }
     const user = await User.create({ name, email, password });
     const tokenUser = { name: user.name, id: user._id, role: user.role };
     const token = user.createToken({ payload: tokenUser });
     res.cookie("token", token, {
       httpOnly: true,
       expires: new Date(Date.now() + 86400000),
+      secure: process.env.NODE_ENV === "production",
+      signed: true,
     });
     res.status(201).json({ user: tokenUser });
   } catch (error) {
@@ -28,6 +29,26 @@ const register = async (req, res, next) => {
 };
 
 const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new CustomError("Please enter all values.", 400));
+  }
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(new CustomError("Email doesn't exist.", 400));
+    }
+
+    const validPassword = await user.comparePassword(password);
+
+    if (!validPassword) {
+      return next(new CustomError("Password is invalid.", 401));
+    }
+  } catch (error) {
+    next(error);
+  }
+
   res.send("Login");
 };
 
