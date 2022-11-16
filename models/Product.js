d5e20186e78c37884ed5986b6a0403345138191d
Christopher Hage <hage.chris@yahoo.com>
2022-11-15 23:29:59 -0400

    product model fields added

---
+++ models/Product.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "Please enter a product name"],
+    maxlength: [100, "Name cannot be longer than 100 characters."],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    default: 0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+    maxlength: [1000, "Description cannot be longer than 1000 characters"],
+  },
+  image: {
+    type: String,
+    default: "/uploads/example.jpeg",
+  },
+  category: {
+    type: String,
+    required: [true, "Please enter category"],
+    enum: ["bedroom", "office", "kitchen"],
+  },
+  company: {
+    type: String,
+    required: [true, "Please enter company"],
+    enum: {
+      values: ["liddy", "marcos", "ikea"],
+      message: "{VALUE} is not available",
+    },
+  },
+  colors: {
+    type: [String],
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model("Product", ProductSchema);
