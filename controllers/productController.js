ec8a2687a3b3dc86633bd931c076d8514e4bd34e
Christopher Hage <hage.chris@yahoo.com>
2022-11-29 22:40:00 -0400

    bug fixes and get product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,17 @@
 import CustomError from "../error/custom-error.js";
 import Product from "../models/Product.js";
 
-const createProduct = async (req, res) => {
+const createProduct = async (req, res, next) => {
   req.body.user = req.user.id;
   try {
-    const product = Product.create(req.body);
+    const product = await Product.create(req.body);
     res.status(200).json({ product });
   } catch (error) {
     next(error);
   }
 };
 
-const getProducts = async (req, res) => {
+const getProducts = async (req, res, next) => {
   try {
     const products = await Product.find({});
     res.status(200).json({ products, length: products.length });
@@ -20,8 +20,14 @@ const getProducts = async (req, res) => {
   }
 };
 
-const getProduct = async (req, res) => {
-  res.send(".");
+const getProduct = async (req, res, next) => {
+  const productID = req.params.id;
+  try {
+    const product = await Product.findOne({ _id: productID });
+    res.status(200).json({ product });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const editProduct = async (req, res) => {
